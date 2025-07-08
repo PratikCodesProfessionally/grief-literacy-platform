10eef0f9d8c44805ef998249a1cb203c4ba18733
PratikCodesProfessionally <career.pratikdevkota@gmail.com>
2025-07-08 05:28:18 +0000

    Express 4 instead of Express 5

--- dist/server/static-serve.mjs
+++ dist/server/static-serve.mjs
@@ -8,16 +8,6 @@ export function setupStaticServing(app) {
     const staticDir = path.join(process.cwd(), 'dist');
     console.log('Registering static assets at:', staticDir);
     app.use(express.static(staticDir));
-    // Fallback-Route für SPA-Routing, aber keine API-Routen überschreiben
-    /*app.use((req, res, next) => {
-      if (req.method !== 'GET' || req.path.startsWith('/api/')) {
-        return next();
-      }
-      res.sendFile(path.join(staticDir, 'index.html'), err => {
-        if (err) next();
-      });
-    });
-  }*/
     // Fallback-Route NUR in Produktion
     if (process.env.NODE_ENV === 'production') {
         app.get('*', (req, res) => {
