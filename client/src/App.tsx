114c5d66406519d48e1aaa638e23acdc24346f4f
PratikCodesProfessionally <career.pratikdevkota@gmail.com>
2025-10-30 12:02:59 +0000

    Something went wrong useToast must be used within ToastProvider
    Reload Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,23 +6,26 @@ import { TherapyPage } from '@/pages/therapy/TherapyPage';
 import { CommunityPage } from '@/pages/community/CommunityPage';
 import { ToolsPage } from '@/pages/tools/ToolsPage';
 import { ResourcesPage } from '@/pages/resources/ResourcesPage';
+import { ToastProvider } from '@/components/ui/use-toast';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gradient-to-br from-blue-50 to-purple-50 dark:from-gray-900 dark:to-gray-800">
-        <Header />
-        <main className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/therapy/*" element={<TherapyPage />} />
-            <Route path="/community/*" element={<CommunityPage />} />
-            <Route path="/tools/*" element={<ToolsPage />} />
-            <Route path="/resources/*" element={<ResourcesPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <ToastProvider>
+      <Router>
+        <div className="min-h-screen bg-gradient-to-br from-blue-50 to-purple-50 dark:from-gray-900 dark:to-gray-800">
+          <Header />
+          <main className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/therapy/*" element={<TherapyPage />} />
+              <Route path="/community/*" element={<CommunityPage />} />
+              <Route path="/tools/*" element={<ToolsPage />} />
+              <Route path="/resources/*" element={<ResourcesPage />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </ToastProvider>
   );
 }
 
